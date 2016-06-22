58e922e6db9c16972557c9c37120ad92f4704212
Aurelien Broszniowski <aurelien@jsoft.biz>
2016-06-22 16:18:58 +0200

    remove precalculation

---
+++ src/main/java/io/rainfall/ReportCompare.java
@@ -0,0 +1,16 @@
+package io.rainfall;
+
+/**
+ *
+ * Compare html reports
+ *
+ * @author Aurelien Broszniowski
+ */
+public class ReportCompare {
+
+  public static void main(String[] args) {
+
+  }
+
+
+}
