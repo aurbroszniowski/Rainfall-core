5b3f5e6b987ea41e16561ee7dc4420a3ae3ff47d
Kevin Cleereman <Kevin.Cleereman@softwareag.com>
2016-08-18 06:49:00 -0600

    Added statistics collectors for memory and processor utilization

---
+++ src/main/java/io/rainfall/statistics/monitor/MemStatisticsCollector.java
@@ -0,0 +1,127 @@
+package io.rainfall.statistics.monitor;
+
+import io.rainfall.reporting.HtmlReporter;
+import io.rainfall.statistics.collector.StatisticsCollector;
+import io.rainfall.statistics.exporter.Exporter;
+import io.rainfall.statistics.exporter.HtmlExporter;
+import io.rainfall.statistics.exporter.TextExporter;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.Writer;
+import java.lang.management.ManagementFactory;
+import java.lang.management.MemoryMXBean;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Queue;
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.concurrent.ConcurrentLinkedQueue;
+
+/**
+ * Created by KECL on 8/18/2016.
+ */
+public class MemStatisticsCollector  implements StatisticsCollector {
+
+  private static final MemoryMXBean MEM_BEAN = ManagementFactory.getMemoryMXBean();
+  private static final String MEM_STATS = "Memory Utilization";
+
+  private final Timer timer = new Timer();
+
+  private Writer memOutput;
+
+  public static class MemStats {
+    public enum Header {
+      MEMORY_UTILIZATION
+    }
+
+    private final long startTimestamp;
+    private final long memUtilization;
+
+    public MemStats(long startTimestamp, long memUtilization) {
+      this.startTimestamp = startTimestamp;
+      this.memUtilization = memUtilization;
+    }
+  }
+
+  private final Queue<MemStatisticsCollector.MemStats> memStatsQueue = new ConcurrentLinkedQueue<MemStatisticsCollector.MemStats>();
+
+  @Override
+  public void initialize() {
+    timer.scheduleAtFixedRate(new TimerTask() {
+      @Override
+      public void run() {
+        memStatsQueue.add(new MemStatisticsCollector.MemStats(System.currentTimeMillis(), MEM_BEAN.getHeapMemoryUsage().getUsed()));
+      }
+    }, 5000, 5000);
+  }
+
+  @Override
+  public void terminate() {
+    timer.cancel();
+  }
+
+  @Override
+  public Exporter peek() {
+    return new MemStatisticsCollector.MemStatisticsExporter();
+  }
+
+  @Override
+  public String getName() {
+    return MEM_STATS;
+  }
+
+  public class MemStatisticsExporter implements HtmlExporter, TextExporter {
+
+    private HtmlReporter reporterUtils = new HtmlReporter();
+    private String memFile = "memory.csv";
+    List<MemStatisticsCollector.MemStats> memStatsList;
+    long jvmStartTime = ManagementFactory.getRuntimeMXBean().getStartTime();
+
+    public MemStatisticsExporter() {
+      memStatsList = new ArrayList<MemStatisticsCollector.MemStats>();
+      while (true) {
+        MemStatisticsCollector.MemStats memStats = memStatsQueue.poll();
+        if (memStats == null) break;
+        memStatsList.add(memStats);
+      }
+    }
+
+    @Override
+    public void ouputText() {
+      //TODO
+    }
+
+    @Override
+    public void ouputCsv(final String basedir) throws Exception {
+      String memFilename = basedir + File.separatorChar + this.memFile;
+
+      memOutput = new BufferedWriter(new FileWriter(memFilename, true));
+      if (new File(memFilename).length() == 0)
+        reporterUtils.addHeader(memOutput, MemStatisticsCollector.MemStats.Header.values());
+
+      for (MemStatisticsCollector.MemStats memStats : this.memStatsList) {
+        memOutput.append(toCsv(memStats)).append("\n");
+      }
+
+      memOutput.close();
+    }
+
+    @Override
+    public String outputHtml() {
+      return "    function reportMemory(filename, title) {\n" +
+             "        $(\"#memory-box\").append(\"<div id='\" + filename + 'memory' + \"' style='height: 550px;width: 1200px;'><div class='title'/><div class='graph'/></div>\");\n" +
+             "        d3.csv(filename + \".csv\", function (data) { processData(data, filename + 'memory', title, 'Memory Utilization') });\n" +
+             "    }\n" +
+             "\n" +
+             "$('body').append('<div class=\"border\"><h1><a name=\"memory\">Memory Utilization</a></h1><div id=\"memory-box\"></div></div><br/>');\n" +
+             "reportMemory('memory', 'Memory Utilization');\n";
+    }
+
+    private String toCsv(MemStatisticsCollector.MemStats memStats) {
+      return String.valueOf(reporterUtils.formatTimestampInNano(jvmStartTime + memStats.startTimestamp)) + "," +
+             memStats.memUtilization;
+    }
+  }
+}
---
+++ src/main/java/io/rainfall/statistics/monitor/OSStatisticsCollector.java
@@ -0,0 +1,127 @@
+package io.rainfall.statistics.monitor;
+
+import io.rainfall.reporting.HtmlReporter;
+import io.rainfall.statistics.collector.StatisticsCollector;
+import io.rainfall.statistics.exporter.Exporter;
+import io.rainfall.statistics.exporter.HtmlExporter;
+import io.rainfall.statistics.exporter.TextExporter;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.Writer;
+import java.lang.management.ManagementFactory;
+import java.lang.management.OperatingSystemMXBean;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Queue;
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.concurrent.ConcurrentLinkedQueue;
+
+/**
+ * Created by KECL on 8/18/2016.
+ */
+public class OSStatisticsCollector implements StatisticsCollector {
+
+  private static final OperatingSystemMXBean OS_BEAN = ManagementFactory.getOperatingSystemMXBean();
+  private static final String OS_STATS = "Processor Utilization";
+
+  private final Timer timer = new Timer();
+
+  private Writer osOutput;
+
+  public static class OSStats {
+    public enum Header {
+      PROCESSOR_UTILIZATION
+    }
+
+    private final long startTimestamp;
+    private final double procUtilization;
+
+    public OSStats(long startTimestamp, double procUtilization) {
+      this.startTimestamp = startTimestamp;
+      this.procUtilization = procUtilization;
+    }
+  }
+
+  private final Queue<OSStatisticsCollector.OSStats> osStatsQueue = new ConcurrentLinkedQueue<OSStatisticsCollector.OSStats>();
+
+  @Override
+  public void initialize() {
+    timer.scheduleAtFixedRate(new TimerTask() {
+      @Override
+      public void run() {
+        osStatsQueue.add(new OSStats(System.currentTimeMillis(), OS_BEAN.getSystemLoadAverage() / OS_BEAN.getAvailableProcessors() * 100.0));
+      }
+    }, 5000, 5000);
+  }
+
+  @Override
+  public void terminate() {
+    timer.cancel();
+  }
+
+  @Override
+  public Exporter peek() {
+    return new OSStatisticsCollector.OSStatisticsExporter();
+  }
+
+  @Override
+  public String getName() {
+    return OS_STATS;
+  }
+
+  public class OSStatisticsExporter implements HtmlExporter, TextExporter {
+
+    private HtmlReporter reporterUtils = new HtmlReporter();
+    private String osFile = "os.csv";
+    List<OSStatisticsCollector.OSStats> osStatsList;
+    long jvmStartTime = ManagementFactory.getRuntimeMXBean().getStartTime();
+
+    public OSStatisticsExporter() {
+      osStatsList = new ArrayList<OSStatisticsCollector.OSStats>();
+      while (true) {
+        OSStatisticsCollector.OSStats osStats = osStatsQueue.poll();
+        if (osStats == null) break;
+        osStatsList.add(osStats);
+      }
+    }
+
+    @Override
+    public void ouputText() {
+      //TODO
+    }
+
+    @Override
+    public void ouputCsv(final String basedir) throws Exception {
+      String osFilename = basedir + File.separatorChar + this.osFile;
+
+      osOutput = new BufferedWriter(new FileWriter(osFilename, true));
+      if (new File(osFilename).length() == 0)
+        reporterUtils.addHeader(osOutput, OSStatisticsCollector.OSStats.Header.values());
+
+      for (OSStatisticsCollector.OSStats osStats : this.osStatsList) {
+        osOutput.append(toCsv(osStats)).append("\n");
+      }
+
+      osOutput.close();
+    }
+
+    @Override
+    public String outputHtml() {
+      return "    function reportOS(filename, title) {\n" +
+             "        $(\"#os-box\").append(\"<div id='\" + filename + 'os' + \"' style='height: 550px;width: 1200px;'><div class='title'/><div class='graph'/></div>\");\n" +
+             "        d3.csv(filename + \".csv\", function (data) { processData(data, filename + 'os', title, 'Processor Utilization') });\n" +
+             "    }\n" +
+             "\n" +
+             "$('body').append('<div class=\"border\"><h1><a name=\"os\">Processor Utilization</a></h1><div id=\"os-box\"></div></div><br/>');\n" +
+             "reportOS('os', 'Processor Utilization');\n";
+    }
+
+    private String toCsv(OSStatisticsCollector.OSStats osStats) {
+      return String.valueOf(reporterUtils.formatTimestampInNano(jvmStartTime + osStats.startTimestamp)) + "," +
+             osStats.procUtilization;
+    }
+  }
+}
